e80a5684990a6b634cb85c251c68980734287e06
Пунько Артем Валерьевич <a.punko@bdc.by>
2018-01-31 16:07:31 +0300

    fix

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
 const app = express();
 
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
+// API file for interacting with MongoDB
+const api = require('./server/routes/api');
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// API location
+app.use('/api', api);
+
+// Send all other requests to the Angular app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+//Set Port
+const port = process.env.PORT || '3001';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
@@ -11,10 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
