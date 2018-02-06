b3df7c650ebd581b6488d74996bb87b0389f3992
Пунько Артем Валерьевич <a.punko@bdc.by>
2018-02-06 18:56:46 +0300

    8

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/api', api);
 
 // Send all other requests to the Angular app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'dist/index.html'));
+// });
 
 //Set Port
 const port = process.env.PORT || '3001';
