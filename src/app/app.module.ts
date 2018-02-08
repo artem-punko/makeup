b0fdd67ad5d2ae98cc3735e02cbd4fa64590181d
artem-punko <artem.punko.dreams@gmail.com>
2018-02-08 23:56:55 +0300

    24

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-// import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import { GalleryModule } from 'ng-gallery';
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import { DataService } from './data.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    GalleryModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [DataService],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
