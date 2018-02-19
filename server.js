5d9de94ddad327215ec6117e84d869163ed40dd9
artem-punko <artem.punko.dreams@gmail.com>
2018-02-19 21:02:59 +0300

    42

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('*', (req, res) => {
 
 app.all('*', function(req, res, next) {
     var origin = req.get('origin'); 
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', "https://stribuk-makeup.herokuapp.com");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
