d6f39abd0cb7b606c90fd8847907bd65f7cf424c
Пунько Артем Валерьевич <a.punko@bdc.by>
2018-02-19 15:36:17 +0000

    40

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ app.get('*', (req, res) => {
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Credentials", true);
     next();
 });
 
