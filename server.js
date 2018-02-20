a4701a3d7d9a58297917a2aa39212e6baaa185ce
Пунько Артем Валерьевич <a.punko@bdc.by>
2018-02-20 11:41:26 +0000

    45

--- server.js
+++ server.js
@@ -1,95 +1,83 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var multer = require('multer');
-
-let response = {
-    status: 200,
-    data: [],
-    message: null
-};
-
-
+var GridFsStorage = require('multer-gridfs-storage');
+var Grid = require('gridfs-stream');
 const path = require('path');
 const http = require('http');
-const api = require('./server/routes/api');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
-const connection = (closure) => {
-    return MongoClient.connect('mongodb://makeup_collection:30secondstomars@ds119258.mlab.com:19258/makeup_collection', (err, db) => {
-        if (err) return console.log(err);
+const router = express.Router();
 
-        closure(db);
-    });
+var app = express();
+
+var options = {
+    server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
 };
+var mongodbUri = 'mongodb://makeup_collection:30secondstomars@ds119258.mlab.com:19258/makeup_collection';
+mongoose.connect(mongodbUri, options);
+var conn = mongoose.connection;
 
-const sendError = (err, res) => {
-    response.status = 501;
-    response.message = typeof err == 'object' ? err.message : err;
-    res.status(501).json(response);
-};
-
+Grid.mongo = mongoose.mongo;
+var gfs = Grid(conn.db);
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/api', api);
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
-
-// app.use(function (req, res, next) {
-//     res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//     next();
-// });
-
-app.all('*', function(req, res, next) {
-    var origin = req.get('origin'); 
-    res.header('Access-Control-Allow-Origin', "https://stribuk-makeup.herokuapp.com");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
     next();
 });
 
-app.use(express.static('../client'));
 app.use(bodyParser.json());
 
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './server/uploads/');
-    },
+var storage = GridFsStorage({
+    gfs: gfs,
     filename: function (req, file, cb) {
         var datetimestamp = Date.now();
         cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1]);
-    }
+    },
+
+    metadata: function (req, file, cb) {
+        cb(null, { originalname: file.originalname });
+    },
+    root: 'notes'
 });
 
 var upload = multer({
     storage: storage
 }).single('file');
 
-
 app.post('/upload', function (req, res) {
     upload(req, res, function (err) {
-        console.log(req)
-        var myobj = { name: req.file.filename, size: req.file.size, type: req.file.mimetype, destination: req.file.destination, originalname: req.file.originalname, typeCollection: req.body.typeCollection};
-        
         if (err) {
             res.json({ error_code: 1, err_desc: err });
             return;
-        } else {
-            connection((db) => {
-                db.collection('notes').insert(myobj);
-            });
         }
+        res.json({ error_code: 0, err_desc: null });
     });
 });
 
-//Set Port
+app.get('/file/:filename', function (req, res) {
+    gfs.collection('notes');
+    gfs.files.find({ filename: req.params.filename }).toArray(function (err, files) {
+        if (!files || files.length === 0) {
+            return res.status(404).json({
+                responseCode: 1,
+                responseMessage: "error"
+            });
+        }
+        var readstream = gfs.createReadStream({
+            filename: files[0].filename,
+            root: "notes"
+        });
+        res.set('Content-Type', files[0].contentType)
+        return readstream.pipe(res);
+    });
+});
+
 const port = process.env.PORT || '3000';
 app.set('port', port);
 
