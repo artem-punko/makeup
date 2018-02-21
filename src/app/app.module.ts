3bc08b83f655373070d311edeceff13715bbfe73
Пунько Артем Валерьевич <a.punko@bdc.by>
2018-02-21 14:28:15 +0000

    53

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+// imports
 import { BrowserModule } from '@angular/platform-browser';
 import { ModuleWithProviders, NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { AppComponent } from './app.component';
-import { HttpModule } from '@angular/http';
-import { ImageService } from './services/image.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GalleryComponent } from './gallery/gallery.component';
-import { RouterModule } from '@angular/router';
-import { ProfileComponent } from './profile/profile.component';
-import { UploadPhotoComponent } from './upload-photo/upload-photo.component';
-// import { FileSelectDirective, FileDropDirective } from 'ng2-file-upload';
-import { FileUploadModule } from 'ng2-file-upload';
-import { Ng2CloudinaryModule } from 'ng2-cloudinary';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Ng2CloudinaryModule } from 'ng2-cloudinary';
+import { FileUploadModule } from 'ng2-file-upload';
+// components
+import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UploadPhotoComponent } from './upload-photo/upload-photo.component';
+// services
+import { ImageService } from './services/image.service';
+
+// routing
 const rootRouting: ModuleWithProviders = RouterModule.forRoot([
   {
     path: 'gallery',
@@ -41,10 +45,10 @@ const rootRouting: ModuleWithProviders = RouterModule.forRoot([
     UploadPhotoComponent
   ],
   imports: [
+    FileUploadModule,
     HttpClientModule ,
     FormsModule,
     Ng2CloudinaryModule,
-    FileUploadModule,
     BrowserModule,
     rootRouting,
     HttpModule,
