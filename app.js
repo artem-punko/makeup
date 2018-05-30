f6657739f8adc897ce132d8af191ce61a90899a4
Punko Artem <artem.punko.dreams@gmail.com>
2018-05-30 15:58:52 +0300

    'qq'

--- app.js
+++ app.js
@@ -10,33 +10,33 @@ var app = express();
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://makeup_collection:30secondstomars@ds119258.mlab.com:19258/makeup_collection', { useMongoClient: true, promiseLibrary: require('bluebird') })
-  .then(() =>  console.log('connection succesful'))
+  .then(() => console.log('connection succesful'))
   .catch((err) => console.error(err));
 
-  app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
 });
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(bodyParser.urlencoded({ 'extended': 'false' }));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/upload', express.static(path.join(__dirname, 'dist')));
 app.use('/gallery', express.static(path.join(__dirname, 'dist')));
 app.use('/profile', express.static(path.join(__dirname, 'dist')));
 app.use('/image', image);
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
@@ -85,7 +85,7 @@ module.exports = app;
 
 
 // app.post('/save', function (req, res) {
- 
+
 // });
 
 // app.get('/upload', (req, res) => {
